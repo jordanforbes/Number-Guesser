144495d58b04e33a520f6f53ffea3343311c21b0
Jordan <Jordforb@gmail.com>
2022-06-05 11:38:26 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// namespace
+namespace NumberGuesser
+{
+    // main class
+    class Program
+    {
+        // entry point method
+        static void Main(string[] args)
+        {
+        }
+    }
+}
