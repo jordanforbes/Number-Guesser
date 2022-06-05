d65c73c5cda4dc5c3c1c15825d85b258700eb152
Jordan <Jordforb@gmail.com>
2022-06-05 11:45:02 -0400

    app vars

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace NumberGuesser
         // entry point method
         static void Main(string[] args)
         {
+            // Set app vars 
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jordan Forbes";
         }
     }
 }
