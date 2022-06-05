3a668061223637c8c1ec22fc0cdfce13bd97289e
Jordan <Jordforb@gmail.com>
2022-06-05 12:21:47 -0400

    return success

--- Program.cs
+++ Program.cs
@@ -27,9 +27,55 @@ namespace NumberGuesser
             Console.WriteLine("What is your name?");
 
             // Get user input
-            string input = Console.ReadLine();
+            string inputName = Console.ReadLine();
 
-            Console.WriteLine("Hello {0}, let's play a game...", input);
+            Console.WriteLine("Hello {0}, let's play a game...", inputName);
+
+            // Init correct number  
+            int correctNumber = 7;
+
+            // Init guess var 
+            int guess = 0;
+
+            // ask user for number
+            Console.WriteLine("Guess a number betwen 1 and 10");
+
+            //while guess is not correct
+            while (guess != correctNumber)
+            {
+                //Get user input
+                string input = Console.ReadLine();
+
+                // cast to int and put in guess 
+                guess = Int32.Parse(input);
+
+                // Match guess to correct number  
+                if (guess != correctNumber)
+                {
+                    // Change text color  
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Write out app info
+                    Console.WriteLine("Wrong Number, please try again");
+
+                    // reset color
+                    Console.ResetColor();
+
+                }
+            }
+
+            // Output success message 
+
+            // Chagne text color 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            //Tell user it's the right number  
+            Console.WriteLine("You are correct!");
+
+            // Reset Text Color 
+            Console.ResetColor();
+            
+
         }
     }
 }
