d83daeae77f316375b3a967f8e5f004fe2b70d63
Jordan <Jordforb@gmail.com>
2022-06-05 12:29:31 -0400

    random number

--- Program.cs
+++ Program.cs
@@ -32,7 +32,13 @@ namespace NumberGuesser
             Console.WriteLine("Hello {0}, let's play a game...", inputName);
 
             // Init correct number  
-            int correctNumber = 7;
+            // int correctNumber = 7;
+
+            // Create a new random object 
+            Random random = new Random();
+            
+            // generate random integer from 1 to 10
+            int correctNumber = random.Next(1, 10);
 
             // Init guess var 
             int guess = 0;
@@ -46,16 +52,34 @@ namespace NumberGuesser
                 //Get user input
                 string input = Console.ReadLine();
 
+                //Make sure it's a number 
+                if (!int.TryParse(input, out guess))
+                {
+                    // Change text color  
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Tell user to guess a number
+                    Console.WriteLine("Please Enter an actual number");
+
+                    // reset color
+                    Console.ResetColor();
+
+                    //keep going 
+                    continue;
+
+                }
+
                 // cast to int and put in guess 
                 guess = Int32.Parse(input);
 
+                // check guess //
                 // Match guess to correct number  
                 if (guess != correctNumber)
                 {
                     // Change text color  
                     Console.ForegroundColor = ConsoleColor.Red;
 
-                    // Write out app info
+                    // tell user to try again
                     Console.WriteLine("Wrong Number, please try again");
 
                     // reset color
@@ -64,9 +88,9 @@ namespace NumberGuesser
                 }
             }
 
-            // Output success message 
+            // Output success message //
 
-            // Chagne text color 
+            // Change text color 
             Console.ForegroundColor = ConsoleColor.Yellow;
 
             //Tell user it's the right number  
