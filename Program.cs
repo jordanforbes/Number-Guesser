5e5261a938947f216b64189b90cad2fe30ac6ea8
Jordan <Jordforb@gmail.com>
2022-06-05 11:51:08 -0400

    input

--- Program.cs
+++ Program.cs
@@ -14,7 +14,22 @@ namespace NumberGuesser
             string appVersion = "1.0.0";
             string appAuthor = "Jordan Forbes";
 
+            // Change text color  
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            // Write out app info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // reset color
+            Console.ResetColor();
+
+            // Ask users name 
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", input);
         }
     }
 }
